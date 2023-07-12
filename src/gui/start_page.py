32674e13c659da7f093ddb872267e07b75072bf4
Carter Vail <cjv1558@rit.edu>
2023-07-12 17:17:32 -0400

    Centered everything and made title larger

--- src/gui/start_page.py
+++ src/gui/start_page.py
@@ -1,31 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
+import tkinter.font as tkFont
 
 class StartPage(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
         # label of frame Layout 2
-        label = ttk.Label(self, text="Start Page")
+        label = ttk.Label(self, text="Mutli-Armed Bandit\nRestaurant Selector", justify="center", font=tkFont.Font(size=36))
 
         # putting the grid in its place by using
+        self.grid_columnconfigure((0, 2), weight=1)
         # grid
-        label.grid(row=0, column=4, padx=10, pady=10)
+        label.grid(row=0, column=1, padx=10, pady=50)
+
 
         button1 = ttk.Button(self, text="Start",
                              command=lambda: controller.set_page("simulation"))
 
         # putting the button in its place by
         # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
+        button1.grid(row=1, column=1, padx=10, pady=10, ipadx=50, ipady=20)
+
 
         ## button to show frame 2 with text layout2
         button2 = ttk.Button(self, text="Options")
 
         # putting the button in its place by
         # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
+        button2.grid(row=2, column=1, padx=10, pady=10, ipadx=50, ipady=20)
+
 
 
         button3 = ttk.Button(self, text="Quit", command=lambda: controller.destroy())
-        button3.grid(row=3, column=1, padx=10, pady=10)
+        button3.grid(row=3, column=1, padx=10, pady=10, ipadx=50, ipady=20)
+
+
+
