bc74722b9a063dc61f6360c6e467266638cd26ba
Carter Vail <cjv1558@rit.edu>
2023-07-25 16:25:05 -0400

    UCB Implementation Started

--- src/simulation/bandits/__init__.py
+++ src/simulation/bandits/__init__.py
@@ -2,10 +2,11 @@ from .bandit_model import BanditModel
 from .random import RandomBandit
 from .epsilon_greedy import EpsilonGreedyBandit
 from .thompson_sampling import TSBandit
+from .ucb import UCBBandit
 
 BANDITS = {
     "Random": RandomBandit,
     "Epsilon Greedy": EpsilonGreedyBandit,
     "Thompson Sampling": TSBandit,
-    "Upper Confidence Bound" : UCBBandit
+    "Upper Confidence Bound": UCBBandit
 }
--- src/simulation/bandits/ucb.py
+++ src/simulation/bandits/ucb.py
@@ -1,6 +1,38 @@
 from .bandit_model import BanditModel
-
+import random
+import math
+import numpy as np
 class UCBBandit(BanditModel):
-    def __init__(self, n_arms: int, epsilon):
-        super().__init__(n_arms)
+    def __init__(self, n_arms: int, epsilon: float = 1):
+        super().__init__(n_arms) #d
         self.type = "Upper Confidence Bound"
+        self.exploration = epsilon #not actually considered epsilon, but epsilon is keyword that BanditModel takes
+        self.sums = [0] * n_arms #sum_rewards
+        self.iter = 0 #t
+        self.count = [0] * n_arms #Nt(a)
+        self.log_hist = []
+
+    def select_arm(self) -> int:
+        UCB_Values = [0] * n_arms
+        bandit = 0
+
+        for i in range(0, self.n_arms):
+            if self.count[i] > 0:
+                ln_iter = math.log(self.iter)
+                self.log_hist.append(ln_iter)
+
+                #Calculate UCB
+                avg = self.sums[i]/self.count[i]
+                ucb_value = avg + self.exploration*(ln_iter/self.count[i])
+                UCB_Values[i] = ucb_value
+
+            elif (self.count[i] == 0):
+                UCB_Values[i] = 5000000000 #so large it will always be selected
+
+        bandit = np.argmax(UCB_Values)
+        return bandit
+
+    def update(self, reward: float, regret: int, choice: int):
+        self.iter += 1
+        self.count[choice] += 1
+        self.sums[choice] += reward
