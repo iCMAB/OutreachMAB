66990a2712d3a1dec7b93f843c6f7ae3e5280beb
Carter Vail <cjv1558@rit.edu>
2023-07-27 14:52:30 -0400

    results page fix, bandit explaination init, removed unused page

---
+++ src/gui/pages/bandit_explain_page.py
@@ -0,0 +1,9 @@
+import tkinter.font as tkFont
+from tkinter import ttk
+
+from src.gui.standard_widgets.page import Page
+
+
+class SettingsExplainPage(Page):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
