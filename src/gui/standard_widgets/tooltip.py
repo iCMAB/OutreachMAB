b7408b51859d75df0c408d99521f6c3768ba7ca5
Dante Falardeau <dfalardeau19@gmail.com>
2023-08-07 20:46:29 -0400

    Added basic mouseover tooltip functionality

---
+++ src/gui/standard_widgets/tooltip.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+
+class ToolTip:
+    def __init__(self, widget, text: str):
+        self.widget = widget
+        self.text = text
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+        widget.bind('<Enter>', self.show)
+        widget.bind('<Leave>', self.hide)
+
+    def show(self, *args):
+        "Display text in tooltip window"
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 57
+        y = y + cy + self.widget.winfo_rooty() + 27
+        self.tipwindow = tw = Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = Label(tw, text=self.text, justify=LEFT,
+                      background="#ffffe0", relief=SOLID, borderwidth=1,
+                      font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hide(self, *args):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
